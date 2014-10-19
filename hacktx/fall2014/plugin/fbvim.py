df1b21d3c52d0eab431b2fb300f2c00243b59be1
Dennis <cheng.dennis@yahoo.com>
2014-10-19 00:51:00 -0500

    two meals later...

--- hacktx/fall2014/plugin/fbvim.py
+++ hacktx/fall2014/plugin/fbvim.py
@@ -1,11 +1,38 @@
 import facebook
+import time
+import threading
 
-def fb_login():
-    return 'work in progress'
+update_stop = threading.Event()
+
+def fb_login(file_name):
+    # TODO: LOG IN
+    meta_file = '.' + file_name + '.likes'
+    new_file = open(meta_file, 'a')
+    new_file.close()
+    # TODO: DRAW LIKES
+    # threading.Timer(0, update).start()
+    update_thread = threading.Thread(target=update).start()
+
+def fb_logout():
+    update_stop.set()
+    # TODO: LOG OUT
+    # TODO: CLEAR LIKES
+    return 'waiting on app review'
 
 def fb_share(user_token, vim_line):
     graph = facebook.GraphAPI(user_token)
-    profile = graph.get_object('me')
-    friends = graph.get_connections('me', 'friends')
-    graph.put_object('me', 'feed', message=vim_line, link='http://www.vexal.us/vim')
-    return 'sharing to facebook'
+    # status_id = str(graph.put_object('me', 'feed', message=vim_line, link='http://www.vexal.us/vim/' + str(vim_line))['id']).split('_')[1]
+    status_attachment = {"name": "User shared some code from Vim", "link":"http://www.vexal.us/vim/" + str(vim_line)}
+    status_id = str(graph.put_wall_post(message=vim_line, attachment=status_attachment)['id']).split('_')[1]
+    return status_id
+
+def get_num_likes(status_id):
+    status = graph.get_object(status_id)
+    return str(len(status['likes']['data']))
+
+def update():
+    count = 0
+    while(not update_stop.is_set()):
+       print count
+       count+=1
+       update_stop.wait(3)
--- hacktx/fall2014/plugin/fbvim.vim
+++ hacktx/fall2014/plugin/fbvim.vim
@@ -13,7 +13,20 @@ python << endOfPython
 
 from fbvim import fb_login
 
-print(FBLogin())
+file_name = str(vim.eval('expand("%:t")')) # for file name only: .split('.')[0]
+fb_login(file_name)
+
+print 'waiting on app review'
+
+endOfPython
+endfunction
+
+function! FBLogout()
+python << endOfPython
+
+from fbvim import fb_logout
+
+print(fb_logout())
 
 endOfPython
 endfunction
@@ -26,9 +39,16 @@ from fbvim import fb_share
 token = 'n/a'
 
 vim_buff = list(vim.current.buffer)
-vim_line = vim_buff[int(vim.eval("a:lineNum")) - 1]
+line_num = int(vim.eval('a:lineNum')) - 1
+vim_line = vim_buff[line_num]
 
-fb_share(token, vim_line)
+print 'sharing line ' + str(line_num + 1) + ' to facebook'
+status_id = fb_share(token, vim_line)
+file_name = str(vim.eval('expand("%:t")'))
+meta_file_name = '.' + file_name + '.likes'
+meta_file = open(meta_file_name, 'a')
+meta_file.write(str(line_num + 1) + ',' + str(status_id) + '\n')
+meta_file.close()
 
 endOfPython
 endfunction
@@ -37,4 +57,5 @@ endfunction
 "  Expose our commands to the user
 " --------------------------------
 command! FBLogin call FBLogin()
+command! FBLogout call FBLogout()
 command! -nargs=1 FBShare call FBShare(<f-args>)
